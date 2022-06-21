a475d62057ef79b3bc40bf96bf754c92003c2602
jennerdulce <jennerdulce@gmail.com>
2022-06-20 18:24:24 -0700

    Completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,38 +14,178 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
+// const people = [
+//   { name: "ford prefect", occupation: "a hitchhiker" },
+//   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+//   { name: "arthur dent", occupation: "a radio employee" }
+// ]
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 
+// Use the basic test format to create a test suite using Jest
+    // describe: Name the test suite appropriately
+    // it: Should return an array with a sentence about each person with their name capitalized.
+    // expect: Invoke the function "createSentence" with the test variables passed in as arguments
+    // toEqual: Pass the expected output as an argument
+
+    describe("createSentence Method", () => {
+      it("Should return an array with a sentence about each person with their name capitalized", () => {
+        const people = [
+          { name: "ford prefect", occupation: "a hitchhiker" },
+          { name: "zaphod beeblebrox", occupation: "a president of the galaxy" },
+          { name: "arthur dent", occupation: "a radio employee" }
+        ]
+        const expected = ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthur Dent is a radio employee."]
+          expect(createSentence(people)).toEqual(expected)
+      })
+  })
+
 // b) Create the function that makes the test pass.
+
+// Create a function
+    // Will name the function "createSentence"
+    // Has 1 parameter that will accept an array, will name this parameter arr
+// Within the function
+    // Use the higher order function .map() on the given array (arr) that is passed in as an argument in order to iterate through all of the objects within the array
+        // Create a variable and set the value to the name property of the current iteration
+          // Use the .split(" ") method with a space passed in as an argument
+        // Now that we have an array that contains both the first and last name. Use the higher order function .forEach() to capitalize the first and last name that are within this array
+          // Declare a value and index within this .forEach() method
+          // Reassign the current indexes value to:
+            // Target the first charavter by using charAt(0) and use the toUpperCase() method to modify the first letter
+            // String concatenate the rest of the string by using the .slice(1) method on the current iteration
+          // Create a new variable to hold the first and lastname.
+            // Set the value to equal splitName.join(" ")
+            // Return a sentence using string interpolation with the full name and the occupation property `${fullName} is a ${value.occupation}.`
+
+
+const createSentence = arr => {
+  let sentences = arr.map(value => {
+    let splitName = value.name.split(" ")
+    splitName.forEach((value, index) => {
+        splitName[index] = value.charAt(0).toUpperCase() + value.slice(1)
+    })
+    let fullName = splitName.join(" ")
+    return `${fullName} is ${value.occupation}.`
+  })
+  return sentences
+}
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+// const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+// const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
+// const test = [0, 0, 0, 0]
 
+// Use the basic test format to create a test suite using Jest
+    // describe: Name the test suite appropriately
+    // it: Should return an array with a sentence about each person with their name capitalized.
+    // expect: Invoke the function "onlyRemainders" with the test variables passed in as arguments
+    // toEqual: Pass the expected output as an argument
+
+    describe("onlyRemainders Method", () => {
+      it("Should return an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+        const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+        const expected1 = [ 2, 0, -1, 0 ]
+        const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+        const expected2 =  [ 2, 1, -1 ]
+        const test = [0, 0, 0, 0]
+        const expected3 = [0, 0, 0, 0]
+        expect(onlyRemainders(hodgepodge1)).toEqual(expected1)
+        expect(onlyRemainders(hodgepodge2)).toEqual(expected2)
+        expect(onlyRemainders(test)).toEqual(expected3)
+      })
+  })
 
 // b) Create the function that makes the test pass.
 
+// const onlyRemainders = arr => {
+//   let listOfNumbers = arr.filter(value => {
+//     if(value == 0){
+//       return 0
+//     } else if(typeof value == 'number') {
+//       return value
+//     }
+//   })
+//   console.log(listOfNumbers)
+//   return listOfNumbers.map(value => {
+//     return value % 3
+//   })
+// }
+
+// Create a function
+    // Will name the function "onlyReminaders"
+    // Has 1 parameter that will accept an array, will name this parameter arr
+// Within the function
+    // Create a variable and set the value to an empty array
+    // Use the higher order function .forEach() on the given array (arr) that is passed in as an argument
+        // Within the .forEach() method, use a conditional to check if each iteration is has a data type of 'number'
+          // If true
+            // push the current iteration into the empty array
+    // Use the .map() on the array that contais all of the numbers
+      // Return the value % 3 for each iteration  
+        // This will create a new array containing the remainders of each iteration
+
+
+const onlyRemainders = arr => {
+  let newArr = []
+  arr.forEach(value => {
+    if(typeof value == 'number'){
+      newArr.push(value)
+    }
+  })
+  return newArr.map(value => {
+    return value % 3
+  })
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
+// const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
+// const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
+
+// Use the basic test format to create a test suite using Jest
+    // describe: Name the test suite appropriately
+    // it: Should return the sum of all the numbers cubed.”
+    // expect: Invoke the function "totalCubed" with the test variables passed in as arguments
+    // toEqual: Pass the expected output as an argument
+
+    describe("totalCubed Method", () => {
+      it("should return the sum of all the numbers cubed.", () => {
+        const cubeAndSum1 = [2, 3, 4]
+        const expected1 = 99
+        const cubeAndSum2 = [0, 5, 10]
+        const expected2 = 1125
+          expect(totalCubed(cubeAndSum1)).toEqual(expected1)
+          expect(totalCubed(cubeAndSum2)).toEqual(expected2)
+      })
+  })
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function
+    // Will name the function "totalCubed"
+    // Has 1 parameter that will accept an array, will name this parameter arr
+// Within the function
+    // Create a variable to hold the total
+    // Use the .forEach() higher order method on the array
+      // Add the value of each iteration to power of 3 to the 'total' variable
+    // return total
+
+const totalCubed = arr => {
+  let total = 0
+  arr.forEach(value => {
+    total = total + Math.pow(value, 3)
+  })
+  return total
+}
--- interview-questions.md
+++ interview-questions.md
@@ -7,44 +7,110 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-  Your answer:
+  Your answer: Luckily this is an easy fix. I have fixed a problem like this by using a migration. Migrations are a tool Active Record in order to make changes to the structure of your database. A few examples are, deleting columns, changing data types of columns, changin columns names and a few others. You also have to provide an area to hold the `foreign key`. The foreign key is used to create a relationship between instances from other tables. Creating a foreign key has a naming convention of the table name that you want followed by `_id`. Here is an example: `student_id`. The foreign key will automatically be updated depending on what student you are creating a new entry for. The foreign key correlates to the `Primary Key` of the desired instance.
 
-  Researched answer:
+  Researched answer: Luckily this is an easy fix. I have fixed a problem like this by using a migration. Migrations are a tool Active Record in order to make changes to the structure of your database. A few examples are, deleting columns, changing data types of columns, changin columns names and a few others. You also have to provide an area to hold the `foreign key`. The foreign key is used to create a relationship between instances from other tables. Creating a foreign key has a naming convention of the table name that you want followed by `_id`. Here is an example: `student_id`. The foreign key will automatically be updated depending on what student you are creating a new entry for. The foreign key correlates to the `Primary Key` of the desired instance. Furthermore, a few things have to happen in order to implement a migration.
 
+  ```
+  add_column :table_name, :column_name, :datatype
+  change_column :table_name, :column_name, :datatype
+  rename_column :table, :old_column, :new_column
+  remove_column :table_name, :column_name
+  ```
+  - After you generate a migration, a new ruby fill will be created. In here, this is where you use a method along with some arguments (examples displayed above)
+  - Changes are not made until a `rails db:migrate` command has been used. This command 'saves' and finialize this migration
 
+  - Reference: [Active Record Migrations](https://github.com/learn-academy-2022-charlie/Syllabus/blob/main/rails/migrations.md)
 
 2. Which RESTful routes must always be passed params? Why?
 
-  Your answer:
+  Your answer: From experience, I know that routes that need parameters are routes where specific instances are wanted such as: 
+  - get / show route which retrieves a single instance from a database
+  - get / edit route which edits a specific post
+  - patch / update route works in conjuntion with the get / edit route in order to make changes
+  - delete / destory route where the id is needed to target a specific instance within a database
+  - Without these parameters, the modifications cannot be made
 
-  Researched answer:
+  Researched answer: From experience, any route that creates or makes any modifications to the database requires a parameter to be able to target a specific instance within the table.
+  - get / show route which retrieves a single instance from a database
+  - get / edit route which edits a specific post
+  - patch / update route works in conjuntion with the get / edit route in order to make changes
+  - delete / destory route where the id is needed to target a specific instance within a database
+  - Without these parameters, the modifications cannot be made
 
-
+ - Reference: [Rails RESTful Routes and CRUD Actions](https://github.com/learn-academy-2022-charlie/Syllabus/blob/main/rails/restful-routes-crud.md)
 
 3. Name three rails generator commands. What is created by each?
 
   Your answer:
+  - `rails g model`
+    - Creates a new table
+  - `rails g migration`
+    - Creates new migration files
+  - `rails g controller`
+    - Creates files needed for MVC
 
   Researched answer:
+  - Each of these generator commands create a couple of files and directories
+  - `rails g model`
+    - This generator command allows us to create a new model / table for out database
+    - With this a few things are created:
+      - db > migrate, creates a new ruby file for the model. This file is made to add certain validations to the model
+      - app > models, also creates a new table for this model to be used
+  - `rails g migration`
+    - db > migrate, a new ruby file that will hold a new migrate command to be performed on the database
+  - `rails g controller`
+    - app > views > main, creates a new directory to store all the views for a specific controller
+    - app > helpers, helper files
+    - app > assets > stylesheets, a scss file to style views
+
+  - Reference: [Active Record](https://github.com/learn-academy-2022-charlie/Syllabus/blob/main/rails/active-record-intro.md)
+  - Reference: [Active Record Migrations](https://github.com/learn-academy-2022-charlie/Syllabus/blob/main/rails/migrations.md)
+  - Reference: [Rails Controllers, Routes, and Views](https://github.com/learn-academy-2022-charlie/Syllabus/blob/main/rails/controllers-routes-views.md)
 
 
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET"    location: /students          
+- `student#index`
+- Display the index page
+- We have been using this route and controller method in order to display all of the instances in a table and perform a Read action
 
-action: "POST"   location: /students       
+action: "POST"   location: /students
+- `student#create`      
+- Creates a new instance within the table
 
 action: "GET"    location: /students/new
+- `student#new`
+- Renders a form that will take the data entered and be used to perform a Create action
 
 action: "GET"    location: /students/2  
+- `student#show`
+- Uses the query passed in the route in order to perform a Read action
 
 action: "GET"    location: /students/2/edit    
+- `student#edit`
+- Used to perform an Update action
 
 action: "PATCH"  location: /students/2      
+- `student#update`
+- Used in conjunction with `student#edit` in order to perform an Update action
 
 action: "DELETE" location: /students/2      
-
+- `student#destory`
+- Uses the query passed in the route in order to perform a `Delete` action
 
 
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+1. As a developer, my list item can have a title, description, and status.
+2. As a developer, I can add a new list item directly to my database
+3. As a developer, I can ensure that all of my list item titles are unique
+4. As a user, I can see all of the list items on the home page
+5. As a user, I can click a list item and be routed to a page where I can see the title, description, and status of this specific list item
+6. As a user, I can click a button to edit a list item
+7. As a user, I can click a button to delete a list item
+8. As a user, I can see a form where I can create a new list item
+9. As a user, I will be directed back to the home page after editing, deleting, or creating a new list item
+10. As a user, I can click a button that will bring me back to the home page, when I am on any other page besides the home page
